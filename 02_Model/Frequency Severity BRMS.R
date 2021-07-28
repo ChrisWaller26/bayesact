c1969c295a6a44e998bf5baf77f576b4fd190393
ChrisWaller26 <61625585+ChrisWaller26@users.noreply.github.com>
2021-07-28 11:55:31 +0100

    Adapted approach to edit template Stan code

--- 02_Model/Frequency Severity BRMS.R
+++ 02_Model/Frequency Severity BRMS.R
@@ -23,11 +23,12 @@ set.seed(123456)
 regions = c("EMEA", "USC")
 
 freq_n = 5e3
-freq_lambda_vec = c(EMEA = 2, USC = 3)
 
 # Defines a non-linear function for lambda to test model still works
 
-lambda_fun = function(expo) exp(-0.5 + 0.01 * expo)
+lambda_fun = function(expo, region){
+  c(EMEA = 1, USC = 2)[region]
+} 
 
 freq_data =
   data.frame(
@@ -40,16 +41,17 @@ freq_data =
     region = sample(regions, freq_n, replace = T)
   ) %>%
   mutate(
-    freq_lambda = freq_lambda_vec[region] * lambda_fun(expo),
+    freq_lambda = lambda_fun(expo, region),
     claimcount_fgu = rpois(freq_n, freq_lambda),
-    loss = 1
+    loss = ded + 10000 # arbitrary loss which will be later ignored by model
   )
 
 #### Simulate severity Data ####
 
-mu_fun = function(expo) 1 * expo ^ 0.05
+mu_fun = function(expo, region){
+  c(EMEA = 7, USC = 8)[region]
+}
 
-sev_mu_vec = c(EMEA = 8, USC = 9)
 sev_sigma_vec = c(EMEA = 1, USC = 1.5)
 
 sev_data =
@@ -61,7 +63,7 @@ sev_data =
     region = rep(freq_data$region,
                  freq_data$claimcount_fgu),
     expo = rep(freq_data$expo,
-                 freq_data$claimcount_fgu)
+               freq_data$claimcount_fgu)
   ) %>%
   mutate(
     loss_uncapped =
@@ -71,8 +73,7 @@ sev_data =
           function(i){
             
             rlnorm(freq_data$claimcount_fgu[i], 
-                   sev_mu_vec[freq_data$region[i]] *
-                     mu_fun(freq_data$expo[i]), 
+                   mu_fun(freq_data$expo[i], freq_data$region[i]), 
                    sev_sigma_vec[freq_data$region[i]])
             
           }
@@ -109,6 +110,7 @@ freq_data_net =
     by = "pol_id"
   ) %>%
   mutate(
+    loss_uncapped = loss,
     lim_exceed = 0,
     claimcount = coalesce(claimcount, 0L)
   )
@@ -121,86 +123,49 @@ full_data =
 
 #### Multivariate Model ####
 
+freq_link = c(mu = "log")
+sev_link = c(mu = "identity", sigma = "log")
+
 fit_freq = 
-  bf(claimcount | subset(freq) ~ f1 + expo * f2,
+  bf(claimcount ~ f1,
      f1 ~ 1 + region,
-     f2 ~ 1,
      nl = TRUE) + 
-  poisson(link = "log")
+  poisson(link = freq_link[["mu"]])
 
 fit_sev = 
-  bf(loss | subset(sev) + trunc(lb = ded) + cens(lim_exceed) ~ 
-       s1 * expo ^ (s2 - dog),
+  bf(loss | trunc(lb = ded) + cens(lim_exceed) ~ 
+       s1 * expo / expo,
      s1 ~ 1 + region,
-     s2 ~ 1,
      sigma ~ 1 + region,
      nl = TRUE
   ) + 
-  lognormal()
-
-
-replace_freq_block <- function(stan_code, 
-                               sev_dist = "lnorm", 
-                               resp_freq = "claimcount",
-                               resp_sev = "loss",
-                               freq_par = c("f1"),
-                               sev_par = c("s1")) {
-  
-  cdf_fun =
-    case_when(
-      sev_dist == "lnorm"  ~ "lognormal_cdf",
-      sev_dist == "gamma"  ~ "gamma_cdf",
-      sev_dist == "norm"   ~ "normal_cdf",
-      sev_dist == "pareto" ~ "pareto_cdf"
-    )
-  
-  sev_par_name =
-    case_when(
-      sev_dist == "lnorm"  ~ c("mu", "sigma"),
-      sev_dist == "gamma"  ~ c("alpha", "beta"),
-      sev_dist == "norm"   ~ c("mu", "sigma"),
-      sev_dist == "pareto" ~ c("y_min", "alpha")
-    )
-    
-  freq_par_name = paste0("mu_", resp_freq) 
-  
-  raw_code <- 
-    stan_code %>% 
-    as.character()
-  
-  ### Modify the mu_claimcount line
-  
-  new_code <- 
-    gsub(
-      str_glue('\\| {freq_par_name}'),
-      str_glue(
-      '\\| {freq_par_name} + 
-        log(1 - {cdf_fun}(
-            ded, 
-            
-            X_{resp_freq}_{freq_par}[, 1:K_{resp_sev}_{sev_par}] * 
-            b_{resp_sev}_{sev_par}, 
-            
-            exp(Intercept_{sev_par_name[2]}_{resp_sev} + 
-            X_{resp_freq}_f1[, 2:K_{sev_par_name[2]}_{resp_sev}] * 
-            b_{sev_par_name[2]}_{resp_sev}
-            )
-      )
-      )'
-      ),
-      raw_code
-    )
-  
-  return(new_code)
-}
+  lognormal(
+    link = sev_link[["mu"]],
+    link_sigma = sev_link[["sigma"]]
+  )
 
 mv_model_formula = fit_sev + fit_freq + set_rescor(FALSE)
 
 stanvars = c(
   stanvar(
-    x = freq_data_net$ded,
+    x = full_data$ded,
     name = "ded"
+  ),
+  stanvar(
+    x = full_data$sev,
+    name = "sev"
+  ),
+  stanvar(
+    x = full_data$freq,
+    name = "freq"
   )
+  # ,
+  # stanvar(
+  #   scode = 
+  #     "b_sigma_loss_Intercept = b_sigma_loss_Intercept + dot_product(means_X_sigma_loss, b_sigma_loss);",
+  #   block = "genquant",
+  #   position = "end"
+  # )
 )
 
 priors = c(prior(normal(0, 1),
@@ -209,11 +174,11 @@ priors = c(prior(normal(0, 1),
                  resp = claimcount,
                  nlpar = f1),
            
-           prior(normal(0, 1),
-                 class = b,
-                 coef = Intercept,
-                 resp = claimcount,
-                 nlpar = f2),
+           # prior(normal(0, 1),
+           #       class = b,
+           #       coef = Intercept,
+           #       resp = claimcount,
+           #       nlpar = f2),
            
            prior(normal(8, 1),
                  class = b,
@@ -221,17 +186,17 @@ priors = c(prior(normal(0, 1),
                  resp = loss,
                  nlpar = s1),
            
-           prior(normal(8, 1),
-                 class = b,
-                 coef = Intercept,
-                 resp = loss,
-                 nlpar = s2),
+           # prior(normal(8, 1),
+           #       class = b,
+           #       coef = Intercept,
+           #       resp = loss,
+           #       nlpar = s2),
            
            prior(lognormal(0, 1),
                  class = Intercept,
                  dpar = sigma,
                  resp = loss)
-           )
+)
 
 mv_model_code =
   make_stancode(
@@ -239,8 +204,7 @@ mv_model_code =
     data = full_data,
     prior = priors,
     stanvars = stanvars
-  ) %>%
-  replace_freq_block()
+  )
 
 mv_model_data =
   make_standata(
@@ -250,9 +214,207 @@ mv_model_data =
     stanvars = stanvars
   )
 
+mv_model_fit <- 
+  brm( formula = mv_model_formula,
+       data = full_data, 
+       prior = priors, 
+       empty = TRUE
+  )
+
+sev_dist = fit_sev$family$family
+freq_dist = fit_freq$family$family
+
+sev_resp = fit_sev$resp
+freq_resp = fit_freq$resp
+
+sev_arg =
+  case_when(
+    sev_dist == "lognormal" ~ c("mu", "sigma"),
+    sev_dist == "gamma"     ~ c("alpha", "beta"),
+    sev_dist == "normal"    ~ c("mu", "sigma"),
+    sev_dist == "pareto"    ~ c("y_min", "alpha")
+  )
+freq_arg = c("mu")
+
+sev_par = setdiff(names(fit_sev$pforms), sev_arg)
+freq_par = setdiff(names(fit_freq$pforms), freq_arg)
+
+inv_logit = function(x) exp(x) / (1 + exp(x))
+
+sev_inv_link = 
+  case_when(
+    sev_link == "identity" ~ "",
+    sev_link == "log"      ~ "exp",
+    sev_link == "logit"    ~ "inv_logit"
+  )
+freq_inv_link = 
+  case_when(
+    freq_link == "identity" ~ "",
+    freq_link == "log"      ~ "exp",
+    freq_link == "logit"    ~ "inv_logit"
+  )
+
+sev_formula = as.character(fit_sev$formula[3])
+freq_formula = as.character(fit_freq$formula[3])
+
+sev_feature_stan = grep(str_glue("C_{sev_resp}_"), 
+                      names(mv_model_data), 
+                      value = TRUE,
+                      fixed = TRUE)
+freq_feature_stan = grep("C_{freq_resp}_", 
+                      names(mv_model_data), 
+                      value = TRUE, 
+                      fixed = TRUE)
+
+## Severity Features
+
+sev_feature =
+  gsub(
+    '[^0-9|A-Z|a-z|_| ]',
+    " ",
+    str_squish(as.character(mv_model_fit$formula$forms[[sev_resp]]$formula[3]))
+  ) %>%
+  str_squish()
+
+for(par in sev_par){
+  
+  sev_feature = trimws(gsub(par, "", sev_feature))
+  
+}
+
+sev_feature = unique(str_split(sev_feature, " ")[[1]])
+
+## Frequency Features
+
+freq_feature =
+  gsub(
+    '[^0-9|A-Z|a-z|_| ]',
+    " ",
+    str_squish(as.character(mv_model_fit$formula$forms[[freq_resp]]$formula[3]))
+  ) %>%
+  str_squish()
+
+for(par in freq_par){
+  
+  freq_feature = trimws(gsub(par, "", freq_feature))
+  
+}
+
+freq_feature = unique(str_split(freq_feature, " ")[[1]])
+
+## Severity Stan Formula
+
+sev_formula_stan = 
+  paste0(
+    sev_inv_link[1],
+    "(",
+    sev_formula,
+    ")"
+    )
+
+if(length(sev_feature_stan) >= 1){
+  
+  for(i in seq(length(sev_feature_stan))){
+    
+    sev_formula_stan =
+      gsub(
+        sev_feature[i],
+        paste0(sev_feature_stan[i], "[n]"),
+        sev_formula_stan
+      )
+    
+  }
+}
+
+## Frequency Stan Formula
+
+freq_formula_stan = 
+  paste0(
+    freq_inv_link[1],
+    "(",
+    freq_formula,
+    ")"
+  )
+
+if(length(freq_feature_stan) >= 1){
+  
+  for(i in seq(length(freq_feature_stan))){
+    
+    freq_formula_stan =
+      gsub(
+        freq_feature[i],
+        paste0(freq_feature_stan[i], "[n]"),
+        freq_formula_stan
+      )
+    
+  }
+  
+}
+
+
+
+## Convert to Stan function format
+
+for(par in sev_par){
+  
+  sev_formula_stan =
+    gsub(
+      par,
+      paste0("nlp_", sev_resp, "_", par, "[n]"),
+      sev_formula_stan
+    )
+  
+}
+
+for(par in freq_par){
+  
+  freq_formula_stan =
+    gsub(
+      par,
+      paste0("nlp_", freq_resp, "_", par, "[n]"),
+      freq_formula_stan
+    )
+  
+}
+
+## Modify Template Code
+
+code_model_template =
+  str_glue(
+    paste(readLines("02_Model/template_model.stan"), collapse = "\n"),
+    .open = "!!{",
+    .close = "}!!"
+    )
+
+adjusted_code = 
+  paste(
+    substr(
+      mv_model_code,
+      1,
+      str_locate(mv_model_code, "model \\{")[, 1] - 1
+    ),
+
+    substr(
+      mv_model_code,
+      str_locate(mv_model_code, "model \\{")[, 1],
+      str_locate(mv_model_code, "for \\(n in 1\\:N_loss\\)")[, 1] - 1
+    ),
+    
+    code_model_template,
+    
+    substr(
+      mv_model_code,
+      str_locate(mv_model_code, "// priors")[, 1],
+      nchar(mv_model_code)
+    ),
+    
+    sep = "\n"
+    )
+
+
 mv_model_fit_stan =
   stan(
-    model_code = mv_model_code,
+    model_code = adjusted_code,
     data = mv_model_data,
     chains = 1,
     iter = 1000,
@@ -260,16 +422,9 @@ mv_model_fit_stan =
     control = 
       list(adapt_delta = 0.999,
            max_treedepth = 15)
-    )
+  )
 
 ## Convert back to BRMS fit object
-
-mv_model_fit <- 
-  brm( formula = mv_model_formula,
-       data = full_data, 
-       prior = priors, 
-       empty = TRUE
-       )
 
 mv_model_fit$fit <- mv_model_fit_stan
 
@@ -286,14 +441,13 @@ save(
   full_data,
   mv_model_fit,
   model_post_samples,
-  file = "Model/mv_model_fit.RData"
+  file = "02_Model/mv_model_fit.RData"
 )
 
-base::load(file = "Model/mv_model_fit.RData")
+base::load(file = "02_Model/mv_model_fit.RData")
 
 sev_output =
   full_data %>%
-  filter(sev) %>%
   mutate(
     mu_pred = 
       posterior_linpred(
@@ -319,7 +473,7 @@ sev_output =
         mv_model_fit,
         resp = "loss",
         dpar = "sigma"
-        ) %>%
+      ) %>%
       colMeans(),
     sigma_pred_q025 = 
       posterior_epred(
@@ -335,7 +489,8 @@ sev_output =
         dpar = "sigma"
       ) %>%
       apply(2, function(x) quantile(x, 0.975))
-  )
+  ) %>%
+  filter(sev)
 
 # This is just a high-level check for reasonableness and does not reflect
 # the "true" posterior of ground up lambdas predicted by the model
@@ -374,22 +529,23 @@ freq_output =
         mv_model_fit,
         newdata = 
           full_data %>%
+          filter(freq) %>%
           left_join(
             sev_output_mean,
             by = "region"
           ) %>%
           mutate(ded = 0),
         resp = "claimcount"
-        ) %>%
+      ) %>%
       colMeans() %>%
-      exp() /
-      (1 - plnorm(ded, mu_pred, sigma_pred)),
+      exp(),
     
     lambda_pred_q025 = 
       posterior_linpred(
         mv_model_fit,
         newdata = 
           full_data %>%
+          filter(freq) %>%
           left_join(
             sev_output_mean,
             by = "region"
@@ -398,14 +554,14 @@ freq_output =
         resp = "claimcount"
       ) %>%
       exp() %>%
-      apply(2, function(x) quantile(x, 0.025)) /
-      (1 - plnorm(ded, mu_pred, sigma_pred)),
+      apply(2, function(x) quantile(x, 0.025)),
     
     lambda_pred_q975 = 
       posterior_linpred(
         mv_model_fit,
         newdata = 
           full_data %>%
+          filter(freq) %>%
           left_join(
             sev_output_mean,
             by = "region"
@@ -414,8 +570,7 @@ freq_output =
         resp = "claimcount"
       ) %>%
       exp() %>%
-      apply(2, function(x) quantile(x, 0.975)) /
-      (1 - plnorm(ded, mu_pred, sigma_pred))
+      apply(2, function(x) quantile(x, 0.975))
   )
 
 model_compare =
@@ -434,9 +589,9 @@ model_compare =
   ungroup() %>%
   left_join(
     sev_output %>%
-    group_by(
-      region
-    ) %>%
+      group_by(
+        region
+      ) %>%
       summarise_at(
         vars(
           mu_pred,
@@ -453,8 +608,12 @@ model_compare =
     by = "region"
   ) %>%
   mutate(
-    mu_actual     = sev_mu_vec[region],
+    mu_actual     = mu_fun(region),
     sigma_actual  = sev_sigma_vec[region],
-    lambda_actual = freq_lambda_vec[region]
+    lambda_actual = lambda_fun(region)
   ) %>%
   select(sort(names(.)))
+
+
+
+
---
+++ 02_Model/template_model.stan
@@ -0,0 +1,46 @@
+
+  for(n in 1:N_!!{sev_resp}!!){
+    
+    mu_!!{sev_resp}!![n] = !!{sev_formula_stan}!!;
+    
+  }
+  
+  for(n in 1:N_!!{sev_resp}!!){
+    
+    mu_!!{freq_resp}!![n] = !!{freq_formula_stan}!!;
+    
+  }
+  
+  for (n in 1:N_!!{sev_resp}!!) {
+      // apply the inverse link function
+      !!{sev_arg[2]}!!_!!{sev_resp}!![n] = !!{sev_inv_link[2]}!!(!!{sev_arg[2]}!!_!!{sev_resp}!![n]);
+    }
+    
+  // Severity
+  
+  for(n in 1:N_!!{sev_resp}!!){
+    
+      if (cens_!!{sev_resp}!![n] == 0) {
+        target += (!!{sev_dist}!!_lpdf(Y_!!{sev_resp}!![n] | mu_!!{sev_resp}!![n], !!{sev_arg[2]}!!_!!{sev_resp}!![n]) -
+        !!{sev_dist}!!_lccdf(lb_!!{sev_resp}!![n] | mu_!!{sev_resp}!![n], !!{sev_arg[2]}!!_!!{sev_resp}!![n])) * sev[n];
+      } else if (cens_!!{sev_resp}!![n] == 1) {
+        target += (!!{sev_dist}!!_lccdf(Y_!!{sev_resp}!![n] | mu_!!{sev_resp}!![n], !!{sev_arg[2]}!!_!!{sev_resp}!![n]) -
+        !!{sev_dist}!!_lccdf(lb_!!{sev_resp}!![n] | mu_!!{sev_resp}!![n], !!{sev_arg[2]}!!_!!{sev_resp}!![n])) * sev[n];
+      } else if (cens_!!{sev_resp}!![n] == -1) {
+        target += (!!{sev_dist}!!_lcdf(Y_!!{sev_resp}!![n] | mu_!!{sev_resp}!![n], !!{sev_arg[2]}!!_!!{sev_resp}!![n]) -
+        !!{sev_dist}!!_lccdf(lb_!!{sev_resp}!![n] | mu_!!{sev_resp}!![n], !!{sev_arg[2]}!!_!!{sev_resp}!![n])) * sev[n];
+      }
+    }
+    
+  for(n in 1:N_!!{freq_resp}!!){
+    
+    target += 
+      freq[n] * 
+        !!{freq_dist}!!_lpmf(Y_!!{freq_resp}!![n]| 
+                      mu_!!{freq_resp}!![n] * (1 - !!{sev_dist}!!_cdf(ded[n], mu_!!{sev_resp}!![n], !!{sev_arg[2]}!!_!!{sev_resp}!![n])));
+    
+  }
+  
+  }
+  
+  
